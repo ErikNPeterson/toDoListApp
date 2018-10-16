9fb6ed5598ae75f7b7d112785cf54ef665b737a2
Erik <erikeh1@gmail.com>
2018-10-16 20:05:06 +0000

    almost finished the app about to add an input field

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import gql from "graphql-tag";
-import {graphql} from 'react-apollo';
+import {graphql, compose} from 'react-apollo';
 import Paper from '@material-ui/core/Paper';
 
 import List from '@material-ui/core/List';
@@ -9,7 +9,7 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
-import CommentIcon from '@material-ui/icons/Comment';
+import CloseIcon from '@material-ui/icons/Close';
 
 const TodosQuery = gql`
 {
@@ -18,29 +18,58 @@ const TodosQuery = gql`
     text
     complete
   }
-}
+} 
 `;
 
-class App extends Component {
-  state = {
-    checked: [0],
-  };
+const UpdateMutation = gql`
+  mutation($id: ID!, $complete: Boolean!) {
+    updateTodo(id: $id, complete: $complete)
+  }
+`;
 
-  handleToggle = value => () => {
-    const { checked } = this.state;
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
+const RemoveMutation = gql`
+  mutation($id: ID!) {
+    removeTodo(id: $id)
+  }
+`;
 
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    this.setState({
-      checked: newChecked,
+// updateTodo does not work yet I need to go back and check this.
+class App extends Component { 
+    updateTodo = async todo => () => {
+    this.props.updateTodo({
+      variables: {
+        id: todo.id,  
+        complete: !todo.complete
+      },
+      update: store => {
+        // Read the data from our cache for this query.
+        const data = store.readQuery({ query: TodosQuery });
+        // Add our comment from the mutation to the end.
+        data.todos = data.todos.map(x => x.id === todo.id ? ({
+          ...todo,
+          complete: !todo.complete,
+        }) : x)
+        // Write our data back to the cache.
+        store.writeQuery({ query: TodosQuery, data });
+      },
     });
   };
+
+  removeTodo = async todo => {
+    this.props.removeTodo({
+      variables: {
+        id: todo.id,  
+      },
+      update: store => {
+        // Read the data from our cache for this query.
+        const data = store.readQuery({ query: TodosQuery });
+        // Add our comment from the mutation to the end.
+        data.todos = data.todos.filter(x => x.id !== todo.id)
+        // Write our data back to the cache.
+        store.writeQuery({ query: TodosQuery, data });
+      },
+    });
+  }
 
   render() {
     const {data: {loading, todos} } = this.props;
@@ -60,7 +89,7 @@ class App extends Component {
               role={undefined}
               dense
               button
-              onClick={this.handleToggle(todo)}
+              onClick={() => this.updateTodo(todo)}
             >
               <Checkbox
                 checked={todo.complete}
@@ -69,8 +98,8 @@ class App extends Component {
               />
               <ListItemText primary={todo.text} />
               <ListItemSecondaryAction>
-                <IconButton aria-label="Comments">
-                  <CommentIcon />
+                <IconButton>
+                  <CloseIcon onClick={() => this.removeTodo(todo)} />
                 </IconButton>
               </ListItemSecondaryAction>
             </ListItem>
@@ -86,4 +115,7 @@ class App extends Component {
 
 
 
-export default graphql(TodosQuery)(App);
+export default compose(
+  graphql(RemoveMutation, {name: 'removeTodo'}),
+  graphql(UpdateMutation, {name:"updateTodo"}),
+  graphql(TodosQuery))(App);
